271c5afb37f631f9dab506a0309ab9b792cd6196
RamonRomeroTec <ramonromeroqro@protonmail.com>
2019-02-05 01:23:14 -0600

    l3

--- lab2/rainbowCircleStart.js
+++ lab2/rainbowCircleStart.js
@@ -19,15 +19,13 @@
     Three.js is distributed under the MIT License
     Copyright © 2010-2019 three.js authors
     
-    Forked from @elizabethZweizig
-
   
     
 */
 
+
 var scene;
 var camera;
-
 initializeScene();
 renderScene();
 
@@ -57,220 +55,82 @@ function initializeScene(){
   camera.lookAt(scene.position);
 
   scene.add(camera);
+  //define more colors and steps
 
-  rainbowCircleGeometry = new THREE.Geometry();
-  
-  // Color definition
+  var angle = 2 * Math.PI / 360;
+  var internalRadius = 0.5;
+  var circle_radius = 3;
+  var number_of_steps = 16;
+  var step = (circle_radius - internalRadius)/number_of_steps;
 
-  black = new THREE.Color('#000000');
-  red = new THREE.Color('#f40202');
-  orange = new THREE.Color('#f49702');
-  yellow = new THREE.Color('#f4db02');
-  green = new THREE.Color('#52f402');
-  blue = new THREE.Color('#0222f4');
-  indigo = new THREE.Color('#4b0082')
+  var colorArray = [new THREE.Color(0,0,0),
+                    new THREE.Color(1,0,0),
+                    new THREE.Color(1,0.5,0),
+                    new THREE.Color(1,1,0),
+                    new THREE.Color(0,1,0),
+                    new THREE.Color(0,0,1),
+                    new THREE.Color(0.5,0,0.5),
+                    new THREE.Color(1,0,1),
+                    new THREE.Color(1,0.7,1),
+                    new THREE.Color(1,0,1),
+                    new THREE.Color(0.5,0,0.5),
+                    new THREE.Color(0,0,1),
+                    new THREE.Color(0,1,0),
+                    new THREE.Color(1,1,0),
+                    new THREE.Color(1,0.5,0),
+                    new THREE.Color(1,0,0),
+                    new THREE.Color(0,0,0)];
+//forming verices and faces
+  for (var j = 0; j <= angle * 360; j += angle){
+    for (var i = 0; i < number_of_steps; i++){
+      var vector1 = new THREE.Vector3(Math.cos(j) * (internalRadius + i * step),
+        Math.sin(j) * (internalRadius + i * step), 0);
+      var vector2 = new THREE.Vector3(Math.cos(j + angle) * (internalRadius + i * step),
+        Math.sin(j + angle) * (internalRadius + i * step), 0);
+      var vector3 = new THREE.Vector3(Math.cos(j) * (internalRadius + (i + 1) * step),
+        Math.sin(j) * (internalRadius + (i + 1) * step), 0);
+      var vector4 = new THREE.Vector3(Math.cos(j + angle) * (internalRadius + (i + 1) * step),
+        Math.sin(j + angle) * (internalRadius + (i + 1) * step), 0);
+      var innerRainbowGeometry = new THREE.Geometry();
 
-  // split circle
-  
-  for( var d=0; d<361; d++ ) {
-    var angle= Math.PI*(d/180);
-    
-    // Fill faces and vertices
+      innerRainbowGeometry.vertices.push(vector2);
+      innerRainbowGeometry.vertices.push(vector1);
+      innerRainbowGeometry.vertices.push(vector3);
+      innerRainbowGeometry.faces.push(new THREE.Face3(0,1,2));
 
-    rainbowCircleGeometry.vertices.push(new THREE.Vector3(Math.sin(angle), Math.cos(angle), 0));
+      innerRainbowGeometry.faces[0].vertexColors[0] = colorArray[i];
+      innerRainbowGeometry.faces[0].vertexColors[1] = colorArray[i];
+      innerRainbowGeometry.faces[0].vertexColors[2] = colorArray[i+1];
 
-    if(rainbowCircleGeometry.vertices.length > 3) {
-      rainbowCircleGeometry.faces.push(new THREE.Face3(
-        rainbowCircleGeometry.vertices.length - 10,
-        rainbowCircleGeometry.vertices.length - 9,
-        rainbowCircleGeometry.vertices.length));
+      var rainbowMaterial = new THREE.MeshBasicMaterial({
+        vertexColors:THREE.VertexColors,
+        side:THREE.DoubleSide
+      });
 
-      rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[0] = black;
-      rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[1] = red;
-      rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[2] = red;
+      var rainbowMesh = new THREE.Mesh(innerRainbowGeometry, rainbowMaterial);
+
+      rainbowMesh.position.set(0.0, 0.0, 0.0);
+      scene.add(rainbowMesh);
+
+      var upperRainbowGeometry = new THREE.Geometry();
+
+      upperRainbowGeometry.vertices.push(vector4);
+      upperRainbowGeometry.vertices.push(vector2);
+      upperRainbowGeometry.vertices.push(vector3);
+      upperRainbowGeometry.faces.push(new THREE.Face3(0,1,2));
+
+      upperRainbowGeometry.faces[0].vertexColors[0] = colorArray[i+1];
+      upperRainbowGeometry.faces[0].vertexColors[1] = colorArray[i];
+      upperRainbowGeometry.faces[0].vertexColors[2] = colorArray[i+1];
+
+      var rainbowMesh2 = new THREE.Mesh(upperRainbowGeometry, rainbowMaterial);
+
+      rainbowMesh2.position.set(0.0, 0.0, 0.0);
+      scene.add(rainbowMesh2);
     }
-
-    rainbowCircleGeometry.vertices.push(new THREE.Vector3(Math.sin(angle)*0.5, Math.cos(angle)*0.5, 0));
-
-    if(rainbowCircleGeometry.vertices.length > 3) {
-      rainbowCircleGeometry.faces.push(new THREE.Face3(
-        rainbowCircleGeometry.vertices.length - 10,
-        rainbowCircleGeometry.vertices.length - 9,
-        rainbowCircleGeometry.vertices.length));
-
-      rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[0] = red;
-      rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[1] = orange;
-      rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[2] = orange;
-
-      rainbowCircleGeometry.faces.push(new THREE.Face3(
-        rainbowCircleGeometry.vertices.length - 10,
-        rainbowCircleGeometry.vertices.length,
-        rainbowCircleGeometry.vertices.length - 1));
-
-      rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[0] = red;
-      rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[1] = orange;
-      rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[2] = red;
-    }
-
-    rainbowCircleGeometry.vertices.push(new THREE.Vector3(Math.sin(angle)*1, Math.cos(angle)*1, 0));
-
-    if(rainbowCircleGeometry.vertices.length > 3) {
-      rainbowCircleGeometry.faces.push(new THREE.Face3(
-	    rainbowCircleGeometry.vertices.length - 10,
-	    rainbowCircleGeometry.vertices.length - 9,
-  	    rainbowCircleGeometry.vertices.length));
-
-      rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[0] = orange;
-	  rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[1] = yellow;
-	  rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[2] = yellow;
-
-      rainbowCircleGeometry.faces.push(new THREE.Face3(
-        rainbowCircleGeometry.vertices.length - 10,
-		rainbowCircleGeometry.vertices.length,
-		rainbowCircleGeometry.vertices.length - 1));
-
-      rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[0] = orange;
-	  rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[1] = yellow;
-	  rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[2] = orange;
-	}
-
-    rainbowCircleGeometry.vertices.push(new THREE.Vector3(Math.sin(angle)*1.5, Math.cos(angle)*1.5, 0));
-
-    if(rainbowCircleGeometry.vertices.length > 3) {
-      rainbowCircleGeometry.faces.push(new THREE.Face3(
-        rainbowCircleGeometry.vertices.length - 10,
-		rainbowCircleGeometry.vertices.length - 9,
-		rainbowCircleGeometry.vertices.length));
-
-      rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[0] = yellow;
-	  rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[1] = green;
-	  rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[2] = green;
-
-      rainbowCircleGeometry.faces.push(new THREE.Face3(
-        rainbowCircleGeometry.vertices.length - 10,
-		rainbowCircleGeometry.vertices.length,
-		rainbowCircleGeometry.vertices.length - 1));
-
-      rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[0] = yellow;
-      rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[1] = green;
-	  rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[2] = yellow;
-    }
-
-    rainbowCircleGeometry.vertices.push(new THREE.Vector3(Math.sin(angle)*2, Math.cos(angle)*2, 0));
-
-    if(rainbowCircleGeometry.vertices.length > 3) {
-      rainbowCircleGeometry.faces.push(new THREE.Face3(
-        rainbowCircleGeometry.vertices.length - 10,
-  	    rainbowCircleGeometry.vertices.length - 9,
-  	    rainbowCircleGeometry.vertices.length));
-
-      rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[0] = green;
-  	  rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[1] = blue;
-  	  rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[2] = blue;
-
-      rainbowCircleGeometry.faces.push(new THREE.Face3(
-        rainbowCircleGeometry.vertices.length - 10,
-  	    rainbowCircleGeometry.vertices.length,
-  	    rainbowCircleGeometry.vertices.length - 1));
-
-      rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[0] = green;
-  	  rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[1] = blue;
-  	  rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[2] = green;
-    }
-
-  	rainbowCircleGeometry.vertices.push(new THREE.Vector3(Math.sin(angle)*2.7, Math.cos(angle)*2.7, 0));
-
-    if(rainbowCircleGeometry.vertices.length > 3) {
-      rainbowCircleGeometry.faces.push(new THREE.Face3(
-        rainbowCircleGeometry.vertices.length - 10,
-  	    rainbowCircleGeometry.vertices.length - 9,
-  	    rainbowCircleGeometry.vertices.length));
-
-      rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[0] = blue;
-      rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[1] = blue;
-      rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[2] = blue;
-
-      rainbowCircleGeometry.faces.push(new THREE.Face3(
-        rainbowCircleGeometry.vertices.length - 10,
-  	    rainbowCircleGeometry.vertices.length,
-  	    rainbowCircleGeometry.vertices.length - 1));
-
-      rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[0] = blue;
-      rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[1] = blue;
-      rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[2] = blue;
-    }
-    
-    
-    
-
-     rainbowCircleGeometry.vertices.push(new THREE.Vector3(Math.sin(angle)*3, Math.cos(angle)*3, 0));
-
-     if(rainbowCircleGeometry.vertices.length > 3) {
-       rainbowCircleGeometry.faces.push(new THREE.Face3(
-         rainbowCircleGeometry.vertices.length - 10,
-         rainbowCircleGeometry.vertices.length - 9,
-         rainbowCircleGeometry.vertices.length));
-
-       rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[0] = blue;
-       rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[1] = indigo;
-       rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[2] = indigo;
-
-       rainbowCircleGeometry.faces.push(new THREE.Face3(
-         rainbowCircleGeometry.vertices.length - 10,
-		 rainbowCircleGeometry.vertices.length,
-		 rainbowCircleGeometry.vertices.length - 1));
-
-       rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[0] = blue;
-	   rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[1] = indigo;
-	   rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[2] = blue;
-    }
-
-
-
-
-    rainbowCircleGeometry.vertices.push(new THREE.Vector3(Math.sin(angle)*3.3, Math.cos(angle)*3.3, 0));
-
-    if(rainbowCircleGeometry.vertices.length > 3) {
-      rainbowCircleGeometry.faces.push(new THREE.Face3(
-        rainbowCircleGeometry.vertices.length - 10,
-		rainbowCircleGeometry.vertices.length - 9,
-		rainbowCircleGeometry.vertices.length));
-
-      rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[0] = indigo;
-	  rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[1] = black;
-	  rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[2] = black;
-
-      rainbowCircleGeometry.faces.push(new THREE.Face3(
-        rainbowCircleGeometry.vertices.length - 10,
-		rainbowCircleGeometry.vertices.length,
-		rainbowCircleGeometry.vertices.length - 1));
-
-      rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[0] = indigo;
-      rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[1] = black;
-      rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[2] = indigo;
-    }
-
-   rainbowCircleGeometry.vertices.push(new THREE.Vector3(Math.sin(angle)*4.2, Math.cos(angle)*4.2, 0));
   }
-  
-  
-  
-  
-  //Create Material
-
-  var rainbowMaterial = new THREE.MeshBasicMaterial({
-    vertexColors:THREE.VertexColors,
-    side:THREE.DoubleSide,
-	//wireframe: true
-  });
-  
-//Create Object
-
-
-  var rainbowMesh = new THREE.Mesh(rainbowCircleGeometry, rainbowMaterial);
-
-  scene.add(rainbowMesh);
 }
+
 
 function renderScene(){
   renderer.render(scene, camera);
